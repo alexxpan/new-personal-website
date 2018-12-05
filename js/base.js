e92cc75dfcbdda9c5e9daa192b81c7f2d65f4805
Alex Pan <alpan@Alexs-MacBook-Pro.local>
2018-12-05 22:34:42 +0100

    added collapsibles for all sections in work

--- js/base.js
+++ js/base.js
@@ -4,17 +4,19 @@ var collapsibles = document.getElementsByClassName("collapsible");
 // Add onClick event for each collapsible
 for (var i = 0; i < collapsibles.length; i++) {
     collapsibles[i].addEventListener("click", function() {
-    var content = this.nextElementSibling;
-    // Toggle content between 0 and max height when clicked
-    if (parseInt(content.style.maxHeight) > 0){
-        content.style.maxHeight = 0;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-        // Scroll to the top of the clicked collapsible
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#contact").offset().top
-        }, 500);
+        var content = this.nextElementSibling;
+        // Toggle content between 0 and max height when clicked
+        if (parseInt(content.style.maxHeight) > 0){
+            content.style.maxHeight = 0;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            // Scroll to the top of the clicked collapsible
+            collapsible_id = this.id;
+            console.log(collapsible_id);
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $("#" + this.id).offset().top
+            }, 500);
 
-    } 
-  });
+        } 
+    });
 }
